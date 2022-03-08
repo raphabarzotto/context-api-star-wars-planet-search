4553b34e62be7bd73c31572d7fd7dc792ce05361
raphabarzotto <raphabarzotto@gmail.com>
2022-03-08 09:51:45 -0300

    feat: set provider in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import StarWarsProvider from './context/StarWarsProvider';
+
 function App() {
   return (
-    <span>Hello, App!</span>
+    <StarWarsProvider>
+      star wars project
+    </StarWarsProvider>
   );
 }
 
